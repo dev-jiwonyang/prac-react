8f6eaf063feddeadd891219c5805ea05105058ae
dev-jiwonyang <jiwonyang@devsisters.com>
2022-06-08 15:37:15 +0900

    react-styling

---
+++ styling-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SassComponent from './component/SassComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <SassComponent />
+    </div>
+  );
+}
+
+export default App;
